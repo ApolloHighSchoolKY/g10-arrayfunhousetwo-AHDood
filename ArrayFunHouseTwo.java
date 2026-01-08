d915f0711fbbe9bf8227f3b9f5c9ff300619837e
AHDood <noe.almanza@stu.daviess.kyschools.us>
2026-01-08 20:07:45 +0000

    commit

--- ArrayFunHouseTwo.java
+++ ArrayFunHouseTwo.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -15,7 +15,10 @@ public class ArrayFunHouseTwo
 	//[9, 11, 13, 8]  returns false
 	public static boolean goingUp(int[] numArray)
 	{
-
+	 for (int i :0; i< numArray )
+	 {
+		if (x)
+	 }
 		return true;
 	}
 
